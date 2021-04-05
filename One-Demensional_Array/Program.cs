f652d8f5f0fe76145ac0167adcc603deb4eed011
Vagner de Souza <inaciovagner1@gmail.com>
2021-04-05 11:24:53 +0100

    Add project files.

---
+++ One-Demensional_Array/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace One_Demensional_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 10, 20, 30, 40, 50 };
+            Array.Sort(a);
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.Write(a[i] + " ");
+            }
+        }
+    }
+}
+
+                   
